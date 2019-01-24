5a2dfc416f6144e17d5ede87dc085195c3c16b65
Plamen Vasilev <plamen.v.vasilev@circles.bz>
2019-01-24 17:17:27 +0200

    add homework

--- Homeworks/6. Dictionaries - Exercises/01. Key-Key Value-Value.py
+++ Homeworks/6. Dictionaries - Exercises/01. Key-Key Value-Value.py
@@ -3,5 +3,10 @@ the_value = input()
 the_n = int(input())
 
 for n in range(0, the_n):
-    line = input()
+    search_key, search_values = input().split(' => ')
+    if the_key in search_key:
+        print(f'{search_key}:')
+        for sub_key in search_values.split(';'):
+            if the_value in sub_key:
+                print(f'-{sub_key}')
 
