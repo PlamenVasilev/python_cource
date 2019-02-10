43ea1edf8a135001687bdae2a7f4d2abbcee9856
Plamen Vasilev <plamen.vasilev.vasilev@gmail.com>
2019-02-10 11:24:28 +0200

    homework

--- Homeworks/8. Objects and Classes/09. Messages.py
+++ Homeworks/8. Objects and Classes/09. Messages.py
@@ -1,20 +1,26 @@
 class User:
     __username: str
-    __messages: list
+    __messages: dict
 
     def __init__(self, username):
         self.__username = username
-        self.__messages = []
+        self.__messages = {}
 
-    def add_message(self, message):
-        self.__messages.append(message)
+    def add_message(self, username, message):
+        if not username in self.__messages:
+            self.__messages[username] = []
+        self.__messages[username].append(message)
 
-    def get_messages(self):
-        return self.__messages
+    def get_messages(self, username):
+        if username in self.__messages:
+            return self.__messages[username]
+        else:
+            return []
 
-    def print_message(self, order, num):
-        if num < len(self.__messages):
-            message = self.__messages[num]
+    def print_message(self, username, order, num):
+        messages = self.get_messages(username)
+        if num < len(messages):
+            message = messages[num]
             if order == 'first':
                 print(f'{self.__username}: {message.get_content()}')
             else:
@@ -39,17 +45,17 @@ while True:
 
     if data == 'exit':
         chats = input()
-        user1, user2 = chats.split()
+        username1, username2 = chats.split()
 
-        user1 = users[user1]
-        user2 = users[user2]
+        user1 = users[username1]
+        user2 = users[username2]
 
-        max_messages = max(len(user1.get_messages()), len(user2.get_messages()))
+        max_messages = max(len(user1.get_messages(username2)), len(user2.get_messages(username1)))
 
         if max_messages > 0:
             for i in range(0, max_messages):
-                user1.print_message("first", i)
-                user2.print_message("second", i)
+                user1.print_message(username2, "first", i)
+                user2.print_message(username1, "second", i)
         else:
             print("No messages")
         break
@@ -67,4 +73,4 @@ while True:
         content = data.split()[3]
 
         if recipient in users and sender in users:
-            users[sender].add_message(Message(recipient, content))
+            users[sender].add_message(recipient, Message(recipient, content))
