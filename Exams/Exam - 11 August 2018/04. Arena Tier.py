3bb2a038cbdcdc2614fae5fde94cda297b8abdcf
Plamen Vasilev <plamen.vasilev.vasilev@gmail.com>
2019-02-15 19:58:19 +0200

    exams

--- Exams/Exam - 11 August 2018/04. Arena Tier.py
+++ Exams/Exam - 11 August 2018/04. Arena Tier.py
@@ -26,10 +26,19 @@ class Gladiator:
 
     def __str__(self):
         prnt = f'{self.get_name()}: {self.get_total_skill()} skill'
-        for technique in sorted(self.__techniques.keys(), reverse=True):
+        for technique in sorted(self.__techniques, key= lambda k: (-self.__techniques[k], k)):
             prnt += f'\n- {technique} <!> {self.__techniques[technique]}'
 
         return prnt
+
+    def get_techniques(self):
+        return list(self.__techniques)
+
+    def get_technique(self, tech):
+        if tech in self.__techniques:
+            return self.__techniques[tech]
+        else:
+            pass
 
 
 gladiators = {}
@@ -37,17 +46,34 @@ while True:
     data = input()
     if data == 'Ave Cesar':
         break
-    
-    name = data.split(' -> ')[0]
-    technique = data.split(' -> ')[1]
-    skill = int(data.split(' -> ')[2])
-    
-    if name in gladiators:
-        gladiators[name].set_skill(technique, skill)
+
+    if data.find('->')>=0:
+        name = data.split(' -> ')[0]
+        technique = data.split(' -> ')[1]
+        skill = int(data.split(' -> ')[2])
+
+        if name in gladiators:
+            gladiators[name].set_skill(technique, skill)
+        else:
+            gladiator = Gladiator(name)
+            gladiator.set_skill(technique, skill)
+            gladiators[name] = gladiator
     else:
-        gladiator = Gladiator(name)
-        gladiator.set_skill(technique, skill)
-        gladiators[name] = gladiator
+        try:
+            gladiator_1 = data.split(' vs ')[0]
+            gladiator_2 = data.split(' vs ')[1]
+            if gladiator_1 in gladiators and gladiator_2 in gladiators:
+                common_techniques = list(set(gladiators[gladiator_1].get_techniques()) & set(gladiators[gladiator_2].get_techniques()))
+                if len(common_techniques):
+                    for tech in common_techniques:
+                        if gladiators[gladiator_1].get_technique(tech) > gladiators[gladiator_2].get_technique(tech):
+                            del gladiators[gladiator_2]
+                        elif gladiators[gladiator_1].get_technique(tech) < gladiators[gladiator_2].get_technique(tech):
+                            del gladiators[gladiator_1]
+                        else:
+                            pass
+        except:
+            pass
 
 gladiators = {k: v for k, v in sorted(gladiators.items(), key=lambda kv: kv[1].get_total_skill(), reverse=True)}
 for name, gladiator in gladiators.items():
